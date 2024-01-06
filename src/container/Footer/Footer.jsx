775115585de427f6eaee08d9dbb65c2166a2c53a
Marcos Zorzi Rosa <marcoszorzi@gmail.com>
2024-01-06 13:03:02 -0500

    Footer components
    Overlay and Newsletter completed.

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import {FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi';
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding'>
+    <FooterOverlay/>
+    <Newsletter/>
   </div>
 );
 
 export default Footer;
+
+/**
+ * Stopped at: 6:15:12
+ * https://www.youtube.com/watch?v=F627pKNUCVQ&t=499s
+ */
